06f2e5fc108c459ad0b095aa867e0b428f1dc51d
Andrew Hinga <andrewhinga5@gmail.com>
2018-10-24 18:29:32 +0300

    [Chore #161413678] Set up database url and create tables

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask, Blueprint
+
+# from flask_jwt_extended import JWTManager
+
+from instance.config import app_config
+
+def create_app(config_name):
+	app = Flask(__name__, instance_relative_config=True)
+	# jwt = JWTManager(app)
+
+	from .api.v1 import version2 as v2
+
+	app.register_blueprint(v2)
+
+	app.config.from_object(app_config[config_name])
+	app.config.from_pyfile('config.py')
+
+	return app
---
+++ instance/config.py
@@ -0,0 +1,39 @@
+import os
+
+class Config(object):
+    """Parent configuration class."""
+    DEBUG = False
+    CSRF_ENABLED = True
+    DATABASE_NAME = 'store_manager'
+    
+
+class DevelopmentConfig(Config):
+    """Configuration fro Development."""
+    DEBUG = True
+    DATABASE_NAME = 'store_manager'
+    JWT_SECRET_KEY = "gvhbjnkmsjbknmlnjk"
+
+class TestingConfig(Config):
+    """Configuration for Testing."""
+    TESTING = True
+    DEBUG = True
+    DATABASE_NAME = "test_storemanager"
+    JWT_SECRET_KEY = "gvhbjnkmsjbknmlnjk"
+
+class StagingConfig(Config):
+    """Configuration for Staging."""
+    DEBUG = False
+    JWT_SECRET_KEY = "gvhbjnkmsjbknmlnjk"
+
+class ProductionConfig(Config):
+    """Configration for Production"""
+    DEBUG = False
+    TESTING = False
+    JWT_SECRET_KEY = "gvhbjnkmsjbknmlnjk"
+
+app_config = {
+    'development' : DevelopmentConfig,
+    'testing' : TestingConfig,
+    'staging' : StagingConfig,
+    'production' : ProductionConfig
+}
