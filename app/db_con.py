57df4cdd2f9d853f9f3474f2bc16e38bfd1a6c60
Andrew Hinga <andrewhinga5@gmail.com>
2018-11-02 16:44:31 +0300

    [Bug #161678968] Fix error in heroku [ci skip]

--- app/db_con.py
+++ app/db_con.py
@@ -7,14 +7,13 @@ from psycopg2.extras import RealDictCursor
 
 class Database:
     '''Class for database connection'''
-    conn = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
     cursor = None
     app = None
 
     def init_app(self, app):
         '''create the database connection'''
         self.app = app
-        self.conn = conn
+        self.conn = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
         # self.conn = psycopg2.connect(
         # 	dbname=app.config['DATABASE_NAME'],
         # 	user=os.getenv("user"),
