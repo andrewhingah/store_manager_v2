9804784107a4459191a34cc260c64bc1879e3877
Andrew Hinga <andrewhinga5@gmail.com>
2018-11-07 11:16:07 +0300

    [Bug #161678968] Restore db connection

--- app/db_con.py
+++ app/db_con.py
@@ -7,17 +7,17 @@ from psycopg2.extras import RealDictCursor
 
 class Database:
     '''Class for database connection'''
+    conn = None
     cursor = None
     app = None
 
     def init_app(self, app):
         '''create the database connection'''
         self.app = app
-        self.conn = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
-        # self.conn = psycopg2.connect(
-        # 	dbname=app.config['DATABASE_NAME'],
-        # 	user=os.getenv("user"),
-        # 	host=os.getenv("host"),
-        # 	password=os.getenv("password")
-        # 	)
+        self.conn = psycopg2.connect(
+        	dbname=app.config['DATABASE_NAME'],
+        	user=os.getenv("user"),
+        	host=os.getenv("host"),
+        	password=os.getenv("password")
+        	)
         self.cursor = self.conn.cursor(cursor_factory=RealDictCursor)
