ddbb81314b6dfd215f66ad298f4dcea09089ab79
Andrew Hinga <andrewhinga5@gmail.com>
2018-11-08 20:10:29 +0300

    [Bug #161678968] Enable cross origin access in app

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 """Module has the create app function"""
 from flask import Flask, Blueprint
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 
 from app.db_con import Database
@@ -12,6 +13,7 @@ from instance.config import app_config
 def create_app(config_name):
 	"""define app and register blueprint"""
 	app = Flask(__name__, instance_relative_config=True)
+	CORS(app)
 
 	jwt = JWTManager(app)
 
